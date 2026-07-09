b32225b8c397ce41ff62f36b8096951e0fc2ed6b
Greenl1s <136644511+Greenl1s@users.noreply.github.com>
2026-07-09 15:18:36 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   // === НАСТРОЙКИ SUPABASE (замените на свои) ===
   supabaseUrl: 'https://ackxeixbpmodxjskdclr.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFja3hlaXhicG1vZHhqc2tkY2xyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1ODgyMzksImV4cCI6MjA5OTE2NDIzOX0.-hs2uQPxLhtfJs2OFf_LD8FH1wQB-OQiwKSLi3hbQZw'
+  supabaseAnonKey: 'sb_publishable_RVjBL6610pQkQnh4KHmxDg_kWiH5kC7'
 };
 
 // Больше не нужны старые настройки прокси и Excel
